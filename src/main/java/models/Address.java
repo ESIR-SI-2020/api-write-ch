583ca591de11432c4a825334fcc88950d0e621b3
Nicolas Bourdin <nicolas.bourdin@capgemini.com>
2019-10-08 13:00:56 +0200

    Version : 0.1 Référence : Changement de mot de passe Commentaire : Setter and getter

--- src/main/java/models/Address.java
+++ src/main/java/models/Address.java
@@ -5,4 +5,36 @@ public class Address {
 	String street;
 	Integer streetNumber;
 	String complement;
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setStreetNumber(Integer streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public Integer getStreetNumber() {
+        return streetNumber;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
 }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -8,6 +8,63 @@ public class User {
 	String email;
 	String photoUrl;
 	String bio;
-	Address address;
-	List<String> friendsId;
+    Address address;
+    List<String> friendsId;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<String> getFriendsId() {
+        return friendsId;
+    }
+
+    public void setFriendsId(List<String> friendsId) {
+        this.friendsId = friendsId;
+    }
+
 }
