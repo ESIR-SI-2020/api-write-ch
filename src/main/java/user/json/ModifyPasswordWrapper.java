8b4fd3312c10fe6fe40e0b0953f7a81aa8f9e625
Nicolas Bourdin <nicolas.bourdin@capgemini.com>
2019-10-14 14:20:41 +0200

    Version : 0.1 Référence : Changement de mot de passe Commentaire : Création du controller

---
+++ src/main/java/user/json/ModifyPasswordWrapper.java
@@ -0,0 +1,14 @@
+package user.json;
+
+public class ModifyPasswordWrapper {
+    private String oldPassword;
+    private String newPassword;
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+}
